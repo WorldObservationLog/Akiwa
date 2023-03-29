2cd9b2e037dd0859293a763a0532ccc47c92eb77
世界观察日志 <wolc@duck.com>
2023-03-29 22:53:48 +0800

    Add enable_local_assets option

--- main.py
+++ main.py
@@ -25,6 +25,21 @@ with saya.module_context():
     saya.require("src.collectors.danmu")
     saya.require("src.receivers.danmu")
 
+if it(Config).config.enable_local_assets:
+    from pathlib import Path
+    if Path("pyecharts-assets").exists():
+        import functools
+        from http.server import HTTPServer, SimpleHTTPRequestHandler
+        class quietServer(SimpleHTTPRequestHandler):
+            def log_message(self, format, *args):
+                pass
+
+        handler = functools.partial(quietServer, directory="pyecharts-assets")
+        httpd = HTTPServer(("0.0.0.0", 8000), handler)
+        it(Broadcast).loop.run_in_executor(None, httpd.serve_forever)
+    else:
+        logger.error("The pyecharts-assets directory does not exists!")
+
 if __name__ == "__main__":
     it(Broadcast).postEvent(CollectorStartEvent(timestamp=int(time.time())))
     it(Broadcast).loop.run_forever()
